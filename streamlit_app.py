304ca4dd23894127c41fb9879eb011afe337e0f6
VIshwas Narayana <vishwasnarayan2345@gmail.com>
2021-04-30 01:43:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
 
 # Retrieving tickers data
 ticker_list = pd.read_csv("constituents_symbols.txt.txt")
-tickerSymbol = st.sidebar.selectbox('Stock ticker', "",ticker_list) # Select ticker symbol
+tickerSymbol = st.sidebar.selectbox('Stock ticker',ticker_list) # Select ticker symbol
 tickerData = yf.Ticker(tickerSymbol) # Get ticker data
 tickerDf = tickerData.history(period='1d', start=start_date, end=end_date) #get the historical prices for this ticker
 
